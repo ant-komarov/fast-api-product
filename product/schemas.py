3cce1910befb5ea7d4d66625e9af17a29d76bfbb
Anton Komarov <anton_komarov_qa@ukr.net>
2023-10-25 17:57:49 +0300

    Add validation for name, description and price

--- product/schemas.py
+++ product/schemas.py
@@ -1,33 +1,42 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
 from category import schemas
 
 
 class ProductBase(BaseModel):
-    name: str
-    description: str
-    price: float
+    name: str = Field(min_length=5, max_length=255, pattern=r'^[a-zA-Z0-9_]+$')
+    description: str =Field(max_length=511)
+    price: float = Field(ge=0)
     quantity: int
 
 
 class ProductCreate(ProductBase):
     category_id: int
+    
+    @validator("price")
+    def round_price(cls, value):
+        return round(value, 2)
 
 
 class ProductUpdate(ProductBase):
-    name: str
-    description: str
-    price: float
+    name: str = Field(min_length=5, max_length=255, pattern=r'^[a-zA-Z0-9_]+$')
+    description: str =Field(max_length=511)
+    price: float = Field(ge=0)
     quantity: int
     category_id: int
-
-
+    
+    
+    @validator("price")
+    def round_price(cls, value):
+        return round(value, 2)
+    
+    
 class Product(ProductBase):
     id: int
     created_at: datetime
-    category: schemas.Category
+    category: schemas.CategoryCreate
 
     class Config:
         from_attributes = True
