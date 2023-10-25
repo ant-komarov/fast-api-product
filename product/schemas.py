ffa5bceb616e3fdc722bbd8e4f834e66242399df
Anton Komarov <anton_komarov_qa@ukr.net>
2023-10-25 18:50:39 +0300

    Refactor code in product->schemas

--- product/schemas.py
+++ product/schemas.py
@@ -6,33 +6,32 @@ from category import schemas
 
 
 class ProductBase(BaseModel):
-    name: str = Field(min_length=5, max_length=255, pattern=r'^[a-zA-Z0-9_]+$')
-    description: str =Field(max_length=511)
+    name: str = Field(min_length=5, max_length=255, pattern=r"^[a-zA-Z0-9_]+$")
+    description: str = Field(max_length=511)
     price: float = Field(ge=0)
     quantity: int
 
 
 class ProductCreate(ProductBase):
     category_id: int
-    
+
     @validator("price")
-    def round_price(cls, value):
+    def round_price(self, value):
         return round(value, 2)
 
 
 class ProductUpdate(ProductBase):
-    name: str = Field(min_length=5, max_length=255, pattern=r'^[a-zA-Z0-9_]+$')
-    description: str =Field(max_length=511)
+    name: str = Field(min_length=5, max_length=255, pattern=r"^[a-zA-Z0-9_]+$")
+    description: str = Field(max_length=511)
     price: float = Field(ge=0)
     quantity: int
     category_id: int
-    
-    
+
     @validator("price")
-    def round_price(cls, value):
+    def round_price(self, value):
         return round(value, 2)
-    
-    
+
+
 class Product(ProductBase):
     id: int
     created_at: datetime
