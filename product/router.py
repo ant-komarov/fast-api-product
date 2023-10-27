321b0d6292d7aa1791d998449417f4cf47b0022c
Anton Komarov <anton_komarov_qa@ukr.net>
2023-10-27 14:13:25 +0300

    Remove unnessessary import

--- product/router.py
+++ product/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from product import schemas, crud
